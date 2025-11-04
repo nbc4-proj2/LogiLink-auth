4843ceeefcd90180f3e59ab0b3e6912d3f0e9626
guswnee00 <guswnee00@gmail.com>
2025-11-05 02:57:49 +0900

    feat(user): 유저 레포

---
+++ src/main/java/com/logilink/auth/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.logilink.auth.repository;
+
+import com.logilink.auth.model.entity.User;
+import java.util.Optional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository{
+
+    void save(User user);
+    Optional<User> findById(Long id);
+    void delete(User user);
+
+    Optional<User> findByUsernameNotDeleted(String username);
+    Optional<User> findByIdNotDeleted(Long userId);
+
+}
