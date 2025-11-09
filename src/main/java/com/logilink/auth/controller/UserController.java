69976b6764bcfa9cc1830485d30492c016f6708d
guswnee00 <guswnee00@gmail.com>
2025-11-09 22:49:47 +0900

    feat(user): 유저 로그인 controller (#4)

--- src/main/java/com/logilink/auth/controller/UserController.java
+++ src/main/java/com/logilink/auth/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.logilink.auth.controller;
 
+import com.logilink.auth.model.dto.request.UserLoginReq;
 import com.logilink.auth.model.dto.request.UserSignupReq;
+import com.logilink.auth.model.dto.response.UserLoginRes;
 import com.logilink.auth.model.dto.response.UserSignupRes;
 import com.logilink.auth.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,15 +14,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/users")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<UserSignupRes>  signup(@RequestBody UserSignupReq userSignupReq) {
+    public ResponseEntity<UserSignupRes> signup(@RequestBody UserSignupReq userSignupReq) {
         return ResponseEntity.ok(userService.signup(userSignupReq));
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserLoginRes> login(@RequestBody UserLoginReq userLoginReq) {
+        UserLoginRes loginRes = userService.login(userLoginReq);
+
+        return ResponseEntity.ok()
+            .header(HttpHeaders.AUTHORIZATION, loginRes.accessToken())
+            .body(loginRes);
+    }
 }
+
 // TODO : 응답 포맷 통일
