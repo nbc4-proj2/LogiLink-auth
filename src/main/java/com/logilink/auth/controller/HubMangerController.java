09d3f50bce05d23f2a15dd70664fc8fc5ea3591a
guswnee00 <guswnee00@gmail.com>
2025-11-10 11:53:54 +0900

    feat(user): 회원가입 승인 controller (#3)

--- src/main/java/com/logilink/auth/controller/HubMangerController.java
+++ src/main/java/com/logilink/auth/controller/HubMangerController.java
@@ -1,47 +1,33 @@
 package com.logilink.auth.controller;
 
 import com.logilink.auth.auth.CustomUserDetails;
-import com.logilink.auth.model.dto.request.MasterSignupReq;
 import com.logilink.auth.model.dto.request.UserStatusUpdateReq;
-import com.logilink.auth.model.dto.response.MasterSignupRes;
 import com.logilink.auth.model.dto.response.UserStatusUpdateRes;
 import com.logilink.auth.model.entity.User;
-import com.logilink.auth.model.entity.UserStatus;
 import com.logilink.auth.service.UserService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/master/users")
+@RequestMapping("/api/v1/hub-manager")
 @RequiredArgsConstructor
-public class MasterController {
+public class HubMangerController {
 
     private final UserService userService;
 
-    @PostMapping("/signup")
-    public ResponseEntity<MasterSignupRes> masterSignup(
-        @RequestHeader("X-SECRET-KEY") String secretKey,
-        @Valid @RequestBody MasterSignupReq masterSignupReq
-    ) {
-        return ResponseEntity.ok(userService.masterSignup(secretKey, masterSignupReq));
-    }
-
-    @PatchMapping("/status")
-    @PreAuthorize("hasRole('MASTER')")
-    public ResponseEntity<UserStatusUpdateRes> updateUserStatusByMaster(
+    @PatchMapping("/users/status")
+    @PreAuthorize("hasRole('HUB_MANAGER')")
+    public ResponseEntity<UserStatusUpdateRes> updateUserStatusByHubManager(
         @AuthenticationPrincipal CustomUserDetails userDetails,
         @RequestBody UserStatusUpdateReq statusUpdateReq
     ) {
         User user = userDetails.user();
-        return ResponseEntity.ok(userService.updateUserStatusByMaster(user, statusUpdateReq));
+        return ResponseEntity.ok(userService.updateUserStatusByHubManager(user, statusUpdateReq));
     }
 }
