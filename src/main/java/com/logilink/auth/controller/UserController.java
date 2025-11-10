75f5584673d4b6206a7a399693efbfcac555f4fb
guswnee00 <guswnee00@gmail.com>
2025-11-10 20:59:41 +0900

    feat(user): 유저 CRUD controller (#5)

--- src/main/java/com/logilink/auth/controller/UserController.java
+++ src/main/java/com/logilink/auth/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.logilink.auth.controller;
 
+import com.logilink.auth.auth.CustomUserDetails;
+import com.logilink.auth.model.dto.UserMyInfo;
 import com.logilink.auth.model.dto.request.UserLoginReq;
 import com.logilink.auth.model.dto.request.UserSignupReq;
 import com.logilink.auth.model.dto.response.UserLoginRes;
 import com.logilink.auth.model.dto.response.UserSignupRes;
+import com.logilink.auth.model.entity.User;
 import com.logilink.auth.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +38,13 @@ public class UserController {
         return ResponseEntity.ok()
             .header(HttpHeaders.AUTHORIZATION, loginRes.accessToken())
             .body(loginRes);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserMyInfo> getUserMyInfo(
+        @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        User user = userDetails.user();
+        return ResponseEntity.ok(userService.getUserMyInfo(user));
     }
 }
