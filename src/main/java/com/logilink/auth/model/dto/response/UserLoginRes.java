9978e2ea896221b5adee6cb743451c2d5db076cd
guswnee00 <guswnee00@gmail.com>
2025-11-12 11:12:52 +0900

    feat(user) : 로그인 시 리프레시 토큰 추가 (#9)

--- src/main/java/com/logilink/auth/model/dto/response/UserLoginRes.java
+++ src/main/java/com/logilink/auth/model/dto/response/UserLoginRes.java
@@ -4,10 +4,15 @@ import com.logilink.auth.model.dto.UserInfo;
 
 public record UserLoginRes(
     String accessToken,
+    String refreshToken,
+    String tokenType,
     long expiresIn,
     UserInfo userInfo
 ) {
-    public static UserLoginRes of(String accessToken, long expiresIn, UserInfo userInfo) {
-        return new UserLoginRes(accessToken, expiresIn, userInfo);
+
+    public static UserLoginRes of(
+        String accessToken, String refreshToken, long expiresIn, UserInfo userInfo
+    ) {
+        return new UserLoginRes(accessToken, refreshToken, "Bearer", expiresIn, userInfo);
     }
 }
