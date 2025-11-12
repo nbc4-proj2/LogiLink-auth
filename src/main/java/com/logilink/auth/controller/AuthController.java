bc1ae12bf62a1f0c72619b6c1b9c3bfa488886be
guswnee00 <guswnee00@gmail.com>
2025-11-13 02:34:41 +0900

    feat(user) : swagger 추가 (#11)

--- src/main/java/com/logilink/auth/controller/AuthController.java
+++ src/main/java/com/logilink/auth/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.logilink.auth.controller;
 import com.logilink.auth.model.dto.request.TokenRefreshReq;
 import com.logilink.auth.model.dto.response.TokenRefreshRes;
 import com.logilink.auth.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -15,10 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/users/auth")
 @RequiredArgsConstructor
+@Tag(name = "User - Token", description = "토큰 갱신 API")
 public class AuthController {
 
     private final UserService userService;
 
+    @Operation(
+        summary = "토큰 갱신",
+        description = "새로운 토큰을 발급합니다."
+    )
     @PostMapping("/refresh")
     public ResponseEntity<TokenRefreshRes> refreshToken(
         @Valid @RequestBody TokenRefreshReq tokenRefreshReq
