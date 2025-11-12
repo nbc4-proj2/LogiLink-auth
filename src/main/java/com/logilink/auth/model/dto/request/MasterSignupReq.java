5f5b790cf8890ff7a2696bd5fce3bee57c993c96
guswnee00 <guswnee00@gmail.com>
2025-11-13 02:35:52 +0900

    feat(user) : swagger (#11)

--- src/main/java/com/logilink/auth/model/dto/request/MasterSignupReq.java
+++ src/main/java/com/logilink/auth/model/dto/request/MasterSignupReq.java
@@ -1,6 +1,7 @@
 package com.logilink.auth.model.dto.request;
 
 import com.logilink.auth.model.dto.UserSignupInfo;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -13,6 +14,7 @@ public record MasterSignupReq(
         regexp = "^[a-z0-9]{4,10}$",
         message = "아이디는 소문자와 숫자로만 구성되며 4자 이상 10자 이하이어야 합니다."
     )
+    @Schema(example = "user1234", description = "4자 이상 10자 이하의 소문자와 숫자로만 구성된 아이디")
     String username,
 
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
@@ -20,10 +22,12 @@ public record MasterSignupReq(
         regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+=-]).{8,15}$",
         message = "비밀번호는 8~15자이며, 알파벳 대소문자, 숫자, 특수문자를 모두 포함해야 합니다."
     )
+    @Schema(example = "ABc1234!", description = "알파벳 대소문자, 숫자, 특수문자를 모두 포함한 8자 이상 15자 이내의 비밀번호")
     String password,
 
     @NotBlank(message = "이메일은 필수 입력 값입니다.")
     @Email(message = "올바른 이메일 형식이 아닙니다.")
     @Size(max = 100, message = "이메일은 100자 이하로 입력해주세요.")
+    @Schema(example = "user1234@email.com", description = "이메일")
     String email
 ) implements UserSignupInfo {}
